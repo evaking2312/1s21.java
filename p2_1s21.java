5784c57b64a91c790b8ab67b3204fcba8223c9cd
evaking2312 <144178875+evaking2312@users.noreply.github.com>
2023-09-05 16:03:25 -0600

    "Practicas 1 y 2" - Martes 05/Septiembre/2023
    Eduardo Valdez Avendaño

---
+++ p2_1s21.java
@@ -0,0 +1,11 @@
+class p2_1s21{
+	public static void main(String[] args) {
+		System.out.println("Datos personales");
+		Nombre.out.println("Eduardo Valdez Avendaño")
+		Edad.out.println("18")
+		Correo.out.println("valdezavendano2gmail.com")
+		Aficion.out.println("estudiante")
+		Color Favorito.out.println("negro y rojo")
+
+	}
+}	
