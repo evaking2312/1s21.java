b703dbe8f25d33c7f498194c29137f2a8cc9e256
evaking2312 <144178875+evaking2312@users.noreply.github.com>
2023-09-26 15:33:03 -0600

    Add files via upload
    Eduardo Valdez Avendaño - "1s21" - (22/09/22023)

---
+++ p10_1s21.java
@@ -0,0 +1,19 @@
+class p10_1s21{
+	public static void main(String[] args); {
+		Stryng opc="a";
+        switch(opc){ 
+       case "a":
+             System.out.printl("aqui el caso a");
+       case "b":
+             System.out.printl("aqui el caso b");
+       case "c": 
+             System.out.printl("aqui el caso c");
+       case "d":
+             System.out.printl("aqui el caso d");
+       case "e":
+             System.out.printl("aqui el caso e");
+       default:
+             System.out.printl("no se encuentra la opcio");
+	     } 
+	}
+}
