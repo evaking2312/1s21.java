39084883d35510cf5c803190cc374ac36cb27e73
Eduardo Valdez Avendaño <144178875+evaking2312@users.noreply.github.com>
2023-10-24 01:49:24 -0600

    Add files via upload
    Eduardo Valdez Avendaño      "1S21"      Practicas 10 y 11.

---
+++ Parcial 2/P11_1S21_P2°.java
@@ -0,0 +1,14 @@
+public class P11_1S21_P2°{
+	public static void main(String[] args) {
+		for (String cadena : args);{
+			char operacion = cadena.charAt(1);
+			switch(operacion){
+		case 'a' System.out.println("inciso a"); break;
+		case 'b' System.out.println("inciso b"); break;
+		case 'h' System.out.println("inciso h"); break;
+		case 'e' System.out.println("inciso e"); break;
+		}
+		}
+
+	}
+}				
